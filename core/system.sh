b7c32fc01dd74e4a1a6fbf741e7fe9663f062276
brandon martin <pie_liker@hotmail.com>
2013-11-07 12:45:11 -0800

    Making it look pretty

--- core/system.sh
+++ core/system.sh
@@ -222,17 +222,8 @@ getOS
 getBarcode
 getName
 
-
-
-rm -f /tmp/hpipe
-mkfifo /tmp/hpipe
-
 # create a background job which takes its input from the named pipe
 $source_dir/extras/CocoaDialog.app/Contents/MacOS/CocoaDialog progressbar --indeterminate --title "Software Updating" --text "Please wait..." < /tmp/hpipe &
-
-# associate file descriptor 3 with that pipe and send a character through the pipe
-exec 5<> /tmp/hpipe
-echo -n . >&5
 
 # do all of your work here
 sudo softwareupdate -i -a
@@ -240,10 +231,4 @@ sudo softwareupdate -i -a
 # update gem versions
 sudo gem update --system
 
-# now turn off the progress bar by closing file descriptor 3
-exec 5>&-
-
-# wait for all background jobs to exit
-wait
-rm -f /tmp/hpipe
 exit 0
