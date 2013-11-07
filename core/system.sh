cc4159b4bc5a1b2d54b639f92ec9e9c79952f04e
brandon martin <pie_liker@hotmail.com>
2013-11-07 12:39:08 -0800

    Making it look pretty

--- core/system.sh
+++ core/system.sh
@@ -228,7 +228,7 @@ rm -f /tmp/hpipe
 mkfifo /tmp/hpipe
 
 # create a background job which takes its input from the named pipe
-~/Desktop/CocoaDialog.app/Contents/MacOS/CocoaDialog progressbar --indeterminate --title "Software Updating" --text "Please wait..." < /tmp/hpipe &
+$source_dir/extras/CocoaDialog.app/Contents/MacOS/CocoaDialog progressbar --indeterminate --title "Software Updating" --text "Please wait..." < /tmp/hpipe &
 
 # associate file descriptor 3 with that pipe and send a character through the pipe
 exec 3<> /tmp/hpipe
