161ff4b6a086f1cc1da03f0c1a7cbc0b487a1cbb
brandon martin <pie_liker@hotmail.com>
2013-11-08 11:34:15 -0800

    Fixing puppet

--- core/defaults.sh
+++ core/defaults.sh
@@ -101,7 +101,7 @@ if [[ ! -f ~/.osx-bootstrap/.osx-bootstrap ]]; then
         echo "$percent Setting Defaults... $percent%"; sleep 0.05
     done > >($source_dir/extras/CocoaDialog.app/Contents/MacOS/CocoaDialog progressbar --title "Defaulting System")
     
-    osascript -e 'tell Application "Finder"' -e 'set the desktop picture to {"$source_dir/extras/Large.jpg"} as alias' -e 'end tell'
+    osascript -e 'tell Application "Finder"' -e 'set the desktop picture to {"~/.osx-bootstrap/extras/Large.jpg"} as alias' -e 'end tell'
 
     for app in Finder Dock Mail Safari iTunes
     do
