bf83d6ce46d884a10d82de16b47f8811e3e6cad1
brandon martin <pie_liker@hotmail.com>
2013-11-08 11:33:48 -0800

    Fixing puppet

--- core/defaults.sh
+++ core/defaults.sh
@@ -108,7 +108,26 @@ if [[ ! -f ~/.osx-bootstrap/.osx-bootstrap ]]; then
         killall "$app" > /dev/null 2>&1
     done
 
-    sudo softwareupdate -i -a >($source_dir/extras/CocoaDialog.app/Contents/MacOS/CocoaDialog progressbar --indeterminate --title "Performing System Updates")
+    rm -f /tmp/hpipe
+    mkfifo /tmp/hpipe
+
+    # create a background job which takes its input from the named pipe
+    $source_dir/extras/CocoaDialog.app/Contents/MacOS/CocoaDialog progressbar --indeterminate --title "My Program" --text "Please wait..." < /tmp/hpipe &
+
+    # associate file descriptor 3 with that pipe and send a character through the pipe
+    exec 3<> /tmp/hpipe
+    echo -n . >&3
+
+    # do all of your work here
+    sudo softwareupdate -i -a
+
+    # now turn off the progress bar by closing file descriptor 3
+    exec 3>&-
+
+    # wait for all background jobs to exit
+    wait
+    rm -f /tmp/hpipe
+    
 
     rv=`$source_dir/extras/CocoaDialog.app/Contents/MacOS/CocoaDialog msgbox --no-newline \
         --text "Take a moment and customize the perferences." \
