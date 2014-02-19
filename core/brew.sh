6bf0064bac77263cf59cb277bdf1841e97fab9fc
brandon martin <pie_liker@hotmail.com>
2014-02-19 10:06:37 -0800

    Still trying to make the brew script work....

--- core/brew.sh
+++ core/brew.sh
@@ -2,23 +2,22 @@
 
 # define helpers
 source_dir=~/.osx-bootstrap
-source $source_dir/core/helpers.sh
-
 
 # create a named pipe
 rm -f $source_dir/tmp/hpipe
 mkdir $source_dir/tmp
 mkfifo $source_dir/tmp/hpipe
 
+log=~/Desktop/Finish\ image\ log/finish-brew-log.sh
+
 # create a background job which takes its input from the named pipe
-$source_dir/extras/CocoaDialog.app/Contents/MacOS/CocoaDialog progressbar --title "Installing Required Scripts" --indeterminate --text "Managing Brew..." < $source_dir/tmp/hpipe &
+$source_dir/extras/CocoaDialog.app/Contents/MacOS/CocoaDialog progressbar --title "Installing Required Scripts" --indeterminate --text "Managing Brew..." < $log &
 
 # associate file descriptor 3 with that pipe and send a character through the pipe
-exec 3<> $source_dir/tmp/hpipe
+exec 3<> $log
 echo -n . >&3
 
 # do all of your work here
-
 
 
 
@@ -26,18 +25,16 @@ echo -n . >&3
 `which -s brew`
 if [[ $? != 0 ]]; then
     echo '##### Installing Homebrew...'
-    ruby -e "$(curl -fsSLk https://raw.github.com/mxcl/homebrew/go/install)" < <(echo 1) &> ~/Desktop/Finish\ image\ logs/finish-brew-log.txt 
-    brew doctor &> ~/Desktop/Finish\ image\ logs/finish-brew-log.txt 
-    brew tap phinze/homebrew-cask &> ~/Desktop/Finish\ image\ logs/finish-brew-log.txt 
+    ruby -e "$(curl -fsSLk https://raw.github.com/mxcl/homebrew/go/install)" < <(echo 1)
+    brew doctor
+    brew tap phinze/homebrew-cask
 else
     echo '##### Running Homebrew Updates...'
-    brew update &> ~/Desktop/Finish\ image\ logs/finish-brew-log.txt 
-    brew doctor &> ~/Desktop/Finish\ image\ logs/finish-brew-log.txt 
+    brew update
+    brew doctor
 fi
 
 exec 3>&-
-
-rm -f $source_dir/tmp/hpipe
 
 # install helpfull formulas
 export formulas='
@@ -55,7 +52,7 @@ do
     ((i += 1))
     if [[ ! $tmp ]]; then
         echo 'Installing Formula '$formula'...'
-        brew install $formula &> ~/Desktop/Finish\ image\ logs/finish-brew-log.txt 
+        brew install $formula
         echo "$i Installing $formula  $i%"; sleep 0.05
     fi
 done > >($source_dir/extras/CocoaDialog.app/Contents/MacOS/CocoaDialog progressbar --indeterminate --title "Installing Required Scripts")
